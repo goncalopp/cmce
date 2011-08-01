15b9547c2d1c30b8d914f7fc1c594b8a39557a1b
goncalopp <goncalopp@gmail.com>
2011-08-01 15:55:19 +0100

    start coding gui logic

--- gui/run.py
+++ gui/run.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python
-
-import sys
-from PyQt4 import QtCore, QtGui
+import sys, os
+from PyQt4 import QtGui
+from PyQt4.QtCore import QObject, QString, SIGNAL
 from main import  Ui_MainWindow
+
+
+
+def event_browse_click():
+    filename= QtGui.QFileDialog().getOpenFileName(None, "Select ISO", "", "ISO image (*.iso)")
+    if filename:
+        myapp.ui.pathEdit.setText(filename)
+
+def event_start_click():
+    filename= myapp.ui.pathEdit.text()
+    m= QtGui.QErrorMessage()
+    if not filename:
+        QtGui.QMessageBox.critical(myapp, "Please select a ISO image", "Please select a ISO image")
+    elif not os.path.exists(filename):
+        QtGui.QMessageBox.critical(myapp, "Could not open the selected ISO image", "Could not open the selected ISO image")
+    else:
+        #all good
+        myapp.ui.progressBar.setValue(100)
+
+
 
 class MyMainWindow(QtGui.QMainWindow):
     def __init__(self, parent=None):
         QtGui.QMainWindow.__init__(self)
         self.ui= Ui_MainWindow()
         self.ui.setupUi(self)
-        #QtCore.QObject.connect(self.ui.pushButton, QtCore.SIGNAL("clicked()"), self.ui.textEdit.clear )
-        #QtCore.QObject.connect(self.ui.lineEdit, QtCore.SIGNAL("returnPressed()"), self.add_entry)
 
-'''
-myapp.ui.progressBar.setValue(4)
-print QtGui.QFileDialog().getOpenFileName(None, "Select ISO", "", "ISO image (*.iso)")
-'''
+    def setupSignals(self):
+        QObject.connect(self.ui.browseButton, SIGNAL("clicked()"), event_browse_click)
+        QObject.connect(self.ui.startButton, SIGNAL("clicked()"), event_start_click)
+
+
+
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
     myapp = MyMainWindow()
+    myapp.setupSignals()
     myapp.show()
     sys.exit(app.exec_())
 
