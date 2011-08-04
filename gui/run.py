b19687111c36241e54fc57cc470112a3f620d59c
goncalopp <goncalopp@gmail.com>
2011-08-04 18:13:46 +0100

    gui : ask languages

--- gui/run.py
+++ gui/run.py
@@ -5,7 +5,7 @@ from PyQt4.QtCore import QObject, QString, SIGNAL
 from main import  Ui_MainWindow
 from gui_library.input  import choice
 from gui_library.output import error
-
+import uck
 
 def check_iso(filename):
     if not filename:
@@ -20,7 +20,11 @@ def start_customization():
     iso_file= get_iso()
     if not check_iso(iso_file):
          return
-
+    if change_language():
+        packs= choice(uck.available_language_packs(), "Which languages do you want your system to support?", multi_choice=True)
+        print packs, "PACKS"
+        boot_languages= choice( uck.available_boot_languages(), "Which languages do you want your system to support while initializing?", multi_choice=True)
+        print boot_languages, "boot"
 
 #------GUI FUNCTIONS-------------------------------------------------
 
@@ -36,6 +40,9 @@ def get_iso():
 def set_progress(fraction):
     assert 0<=fraction<=1
     myapp.progressBar.setValue( int(100*fraction) )
+
+def change_language():
+    return myapp.languageCheck.isChecked()
 
 class MyMainWindow(Ui_MainWindow, QtGui.QMainWindow):
     def __init__(self):
